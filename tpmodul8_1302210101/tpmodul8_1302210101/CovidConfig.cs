dd7666a69f3efc71732fe317cad9245d2aa226e7
MaximusBayu <121425228+MaximusBayu@users.noreply.github.com>
2023-04-09 18:07:15 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302210101/tpmodul8_1302210101/CovidConfig.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302210101
+{
+    internal class CovidConfig
+    {
+        public class set
+        {
+            public string satuan_suhu { get; set; }
+            public int batas_hari_demam { get; set; }
+            public string pesan_ditolak { get; set; }
+            public string pesan_diterima { get; set; }
+
+            public set(string satuan_suhu, int batas_hari_demam, string pesan_ditolak, string pesan_diterima)
+            {
+                this.satuan_suhu = satuan_suhu;
+                this.batas_hari_demam = batas_hari_demam;
+                this.pesan_ditolak = pesan_ditolak;
+                this.pesan_diterima = pesan_diterima;
+            }
+        }
+
+        public class readWrite
+        {
+            public set config;
+            public const string txt = @"./covid_config.json";
+
+            private void SetDefault()
+            {
+                config = new set("celcius", 14,
+                    "Anda tidak diperbolehkan masuk ke dalam gedung ini", 
+                    "Anda dipersilahkan untuk masuk ke dalam gedung ini");
+            }
+
+            private void NewConfigFile()
+            {
+                JsonSerializerOptions baru = new JsonSerializerOptions(){
+                    WriteIndented = true
+                };
+
+                string jsonString = JsonSerializer.Serialize(config, baru);
+                File.WriteAllText(txt, jsonString);
+            }
+
+            private set ReadConfigFile()
+            {
+                string readJSON = File.ReadAllText(txt);
+                config = JsonSerializer.Deserialize<set>(readJSON);
+                return config;
+            }
+
+            public readWrite(){
+                try
+                {
+                    ReadConfigFile();
+                }
+                catch (Exception)
+                {
+                    SetDefault();
+                    NewConfigFile();
+                }
+            }
+
+            public void UbahSatuan(){
+                if (config.satuan_suhu == "celcius"){
+                    config.satuan_suhu = "farenheit";
+                    NewConfigFile();
+                }else{
+                    config.satuan_suhu = "celcius";
+                    NewConfigFile();
+                }
+            }
+        }
+    }
+}
--- tpmodul8_1302210101/tpmodul8_1302210101/Program.cs
+++ tpmodul8_1302210101/tpmodul8_1302210101/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using static tpmodul8_1302210101.CovidConfig;
+
+readWrite rw = new readWrite();
+
+
+Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + rw.config.satuan_suhu);
+double suhu = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+int hari = Convert.ToInt32(Console.ReadLine());
+
+if ((rw.config.satuan_suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) && hari < rw.config.batas_hari_demam){
+    Console.WriteLine(rw.config.pesan_diterima);
+}else if ((rw.config.satuan_suhu == "farenheit") && suhu >= 97.7 && suhu <= 99.5 && hari < rw.config.batas_hari_demam){
+    Console.WriteLine(rw.config.pesan_diterima);
+}else{
+    Console.WriteLine(rw.config.pesan_ditolak);
+}
